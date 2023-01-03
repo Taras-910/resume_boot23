2c14b42f1290c8410efd3fc2feecdd1460e3c14b
taras <promverstat@ukr.net>
2023-01-03 22:10:48 +0200

    01_init

---
+++ src/test/java/ua/training/top/resume/ResumeBoot23ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.training.top.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBoot23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
